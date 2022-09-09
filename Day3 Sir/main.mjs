d4ec2b8a54c5f56773f256c9d4f3d7a220e3b4c7
TarunGupta10 <tarunhl1999@gmail.com>
2022-09-09 22:56:09 +0530

    commit

--- Day3 Sir/main.mjs
+++ Day3 Sir/main.mjs
@@ -1,12 +1,13 @@
 import {Application } from "https://cdnjs.cloudflare.com/ajax/libs/pixi.js/6.5.2/browser/pixi.mjs";
-import { start } from "./js/game.mjs";
+import { start as gameStart} from "./js/game.mjs";
 import { loadAssets } from "./js/loader.mjs";
 
 export function createGame(div) {
     const game = new Application({
         width:innerWidth,
         height:innerHeight,
-        backgroundColor:0xabdbe3
+        backgroundColor:0x2266FF,
+        resizeTo: div
     });
     console.log('game', game);
     div.appendChild(game.view);
@@ -15,9 +16,9 @@ export function createGame(div) {
 
 const game = createGame(document.getElementById('game'));
 loadAssets(game,[
-    { name: "back", url: "/assets/cardback.png" },
+    { name: "back", url: "assets/cardback.png" },
     { name: "front", url: "assets/smilies.jpg" },
   ], () => {
     console.log('starting game');
-    start(game);
-  });   
+    gameStart(game);
+  });
