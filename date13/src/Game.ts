8407f6c07a25b3397e4d0388761633e5e594f085
TarunGupta10 <tarunhl1999@gmail.com>
2022-09-15 01:33:32 +0530

    commit

--- date13/src/Game.ts
+++ date13/src/Game.ts
@@ -1,4 +1,4 @@
-import {Application, Sprite} from 'pixi.js';
+import {Application, Sprite,Text} from 'pixi.js';
 import {gsap} from 'gsap';
 export class Game extends Application{
     private spin:boolean;
@@ -15,6 +15,27 @@ export class Game extends Application{
         this.loader.add(list);
         this.loader.load(cb);
     }
+    winnerpage(luck:number):void{
+        let arr = [200, 400, 800, 1000, 100, 500, 300]
+        let results = ""
+        console.log(arr[luck])
+        if(luck==2 || luck==3 || luck==5){
+            results="you are so lucky"
+        }
+        else{
+            results="yay... you won bonus"
+        }
+        let text = new Text("Yayyy... You have won : "+arr[luck]+"$ from this lucky spin wheel\n"+results)
+        text.x = innerWidth/2
+        text.y = innerHeight/2
+        text.anchor.set(0.5);
+        this.stage.addChild(text)
+        setTimeout(() => {
+            text.visible = false
+            this.onLoad()
+        }, 3000);
+    }
+
     onLoad():void {
         const wheel = new Sprite(this.loader.resources['wheel'].texture);
         wheel.scale.set(1.3)
@@ -41,7 +62,15 @@ export class Game extends Application{
         wheel.on('pointerup', ()=>{
             let random = Math.floor(Math.random()*7);
             let stopAngle = random * this.sliceAngle;
-            gsap.fromTo(wheel,{angle:0},{angle:3600+stopAngle, duration:5, ease:'expo.out'});
+                gsap.fromTo(wheel,{angle:0},{angle:3600+stopAngle, duration:5, ease:'expo.out'});
+
+            wheel.interactive = false
+            setTimeout(() => {
+                wheel.visible=false
+                ptr.visible = false
+                this.winnerpage(random)
+            }, 7000);
+            
         });
     }
 }
