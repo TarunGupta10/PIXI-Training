ffaf554ecf45b8a7bf79e78c9cb15cd74c90fd73
TarunGupta10 <tarunhl1999@gmail.com>
2022-09-16 03:46:05 +0530

    commit

--- date14/src/Game.ts
+++ date14/src/Game.ts
@@ -1,16 +1,16 @@
 import { Application, Loader, Sprite } from "pixi.js";
 import { gsap } from "gsap";
 import { getResource, setResources } from "./Texture.utils";
-import { sound } from '@pixi/sound';
+import { sound } from "@pixi/sound";
 import { Spine } from "pixi-spine";
 export class Game extends Application {
   constructor(opts: any) {
     super(opts);
     this.preload(
       [
-        { name: "goblin", url: "assets/goblins/goblins.json" },
-        { name: "boy", url: "assets/spineboy/spineboy.json" },
-        { name: "hero", url: "assets/hero/hero.json" },
+        { name: "goblin", url: "assets/goblins/goblins-pro.json" },
+        { name: "boy", url: "assets/spineboy/spineboy-pro.json" },
+        { name: "hero", url: "assets/hero/hero-pro.json" },
         // { name: "bg", url: "assets/bg.png" },
       ],
       this.onLoad.bind(this)
@@ -26,31 +26,52 @@ export class Game extends Application {
   onLoad(): void {
     this.stage.interactive = true;
 
-    // const gob = new Spine(getResource("goblin").spineData);
-    // gob.skeleton.setSkinByName("goblin");
-    // gob.skeleton.setSlotsToSetupPose();
-    // gob.state.setAnimation(0, "walk", true);
-    // gob.x = this.screen.width / 2 + 250;
-    // gob.y = this.screen.height - 50;
-    // this.stage.addChild(gob);
+    const bg = Sprite.from("assets/bg.png");
+    bg.width = innerWidth;
+    bg.height = innerHeight;
+    this.stage.addChild(bg);
 
-    // this.stage.on("pointertap", () => {
-    //   // change current skin
-    //   const currentSkinName = gob.skeleton.skin.name;
-    //   const newSkinName =
-    //     currentSkinName === "goblin" ? "goblingirl" : "goblin";
-    //   gob.skeleton.setSkinByName(newSkinName);
-    //   gob.skeleton.setSlotsToSetupPose();
-    // });
-    // 
+    const hero = new Spine(getResource("hero").spineData);
+    // hero.skeleton.setSkinByName("goblin");
+    hero.skeleton.setSlotsToSetupPose();
+    hero.state.setAnimation(0, "walk", true);
+    hero.x = this.screen.width / 2 - 450;
+    hero.y = this.screen.height - 100;
+    hero.scale.set(1);
+    this.stage.addChild(hero);
+    let previousAnim1: string = "walk";
+    let currentAnim1: string = "walk";
+    let revertToAnim1: string = "walk";
+    let direction1: number = -1;
+    var defaultScale = hero.scale.x;
+    this.ticker.add(() => {
+      // hero.scale.x = defaultScale * direction1;
+      if (currentAnim1 != previousAnim1) {
+        console.log(currentAnim1, previousAnim1);
+        hero.state.setAnimation(0, currentAnim1, true);
+        previousAnim1 = currentAnim1;
+      }
+    });
 
-    const bg = Sprite.from("assets/bg.png")
-    bg.width = innerWidth
-    bg.height = innerHeight
-    this.stage.addChild(bg)
+    const gob = new Spine(getResource("goblin").spineData);
+    gob.skeleton.setSkinByName("goblin");
+    gob.skeleton.setSlotsToSetupPose();
+    gob.state.setAnimation(0, "walk", true);
+    gob.x = this.screen.width / 2 + 400;
+    gob.y = this.screen.height - 100;
+    this.stage.addChild(gob);
+
+    this.stage.on("pointertap", () => {
+      // change current skin
+      const currentSkinName = gob.skeleton.skin.name;
+      const newSkinName =
+        currentSkinName === "goblin" ? "goblingirl" : "goblin";
+      gob.skeleton.setSkinByName(newSkinName);
+      gob.skeleton.setSlotsToSetupPose();
+    });
 
     const spineboy = new Spine(getResource("boy").spineData);
-    spineboy.x = this.screen.width / 2 - 250;
+    spineboy.x = this.screen.width / 2;
     spineboy.y = this.screen.height - 80;
     spineboy.scale.set(0.5);
     spineboy.skeleton.setSlotsToSetupPose();
@@ -74,7 +95,7 @@ export class Game extends Application {
     let currentAnim: string = "idle";
     let revertToAnim: string = "idle";
     let direction: number = 1;
-    let defaultScale = spineboy.scale.x;
+    var defaultScale = spineboy.scale.x;
     this.ticker.add(() => {
       spineboy.scale.x = defaultScale * direction;
       if (currentAnim != previousAnim) {
@@ -96,37 +117,77 @@ export class Game extends Application {
           break;
         case "keydown":
           switch (e.code) {
+            // spine boy
             case "KeyW":
               currentAnim = "jump";
               break;
             case "KeyX":
               currentAnim = "death";
+              revertToAnim = "death";
               break;
-              case "KeyH":
-                currentAnim = "hit";
-                break;
-                case "KeyR":
-                currentAnim = "run";
-                break;
+            case "KeyR":
+              currentAnim = "run";
+              revertToAnim = "run";
+              break;
+            case "KeyF":
+              currentAnim = "hoverboard";
+              revertToAnim = "hoverboard";
+              break;
+            case "KeyS":
+              currentAnim = "portal";
+              revertToAnim = "walk";
+              // sound.add("sound2", "assets/portal.mp3");
+              // sound.play("sound2");
+
+              break;
+            case "KeyE":
+              currentAnim = "aim";
+              revertToAnim = "idle";
+              break;
             case "KeyD":
               currentAnim = "walk";
               revertToAnim = "walk";
               direction = 1;
-              // sound.add("sound2","assets/footsteps.mp3")
-              // sound.play("sound2")
               break;
             case "KeyA":
               currentAnim = "walk";
               revertToAnim = "walk";
               direction = -1;
-              // sound.add("sound2","assets/footsteps.mp3")
-              // sound.play("sound2")
               break;
             case "Space":
               currentAnim = "shoot";
-              sound.add("sound1","assets/blaster1.mp3")
-              sound.play("sound1")
+              revertToAnim = "idle";
+              sound.add("sound1", "assets/blaster1.mp3");
+              sound.play("sound1");
               break;
+
+               //Hero
+        case "KeyU":
+          currentAnim1 = "crouch";
+          revertToAnim1 = "idle";
+          break;
+        case "KeyI":
+          currentAnim1 = "run";
+          revertToAnim1 = "run";
+          break;
+          case "KeyK":
+              currentAnim1 = "head-turn";
+              revertToAnim1 = "head-turn";
+              break;
+              case "KeyL":
+                currentAnim1 = "attack";
+                break;
+                case "KeyD":
+              currentAnim1 = "idle";
+              revertToAnim1 = "idle";
+              direction1 = 1;
+              break;
+              case "KeyJ":
+              currentAnim1 = "walk";
+              revertToAnim1 = "walk";
+              direction1 = -1;
+              break;
+
             default:
               currentAnim = "idle";
               revertToAnim = "idle";
